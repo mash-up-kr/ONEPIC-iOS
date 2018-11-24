ca6fed63dd9985429ca3fccd38c038e85551f16b
Daeyun Kim <kim.daeyun@navercorp.com>
2018-11-24 20:25:47 +0900

    init project

---
+++ ONEPIC/ONEPIC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ONEPIC
+//
+//  Created by Daeyun Ethan on 24/11/2018.
+//  Copyright © 2018 Mash-Up. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
